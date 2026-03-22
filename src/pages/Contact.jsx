97b76ead062af2a9f59d905fa72ecb3e01c6f111
Saurabh chaurasiya <85618287+Saurabh1960@users.noreply.github.com>
2026-03-22 21:30:10 +0530

    fdhg

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -8,6 +8,16 @@ function Contact() {
         <textarea className="form-control mb-2" placeholder="Message"></textarea>
         <button className="btn btn-primary">Send</button>
       </form>
+      <a
+        href="https://wa.me/919123456789?text=Hello%20I%20want%20to%20order"
+        className="btn btn-success"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Order on WhatsApp
+      </a>
+
+
     </div>
   )
 }
